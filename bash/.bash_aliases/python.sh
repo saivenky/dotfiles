044defaac97622788105ff1ca770abaf09aa0eab
Sai <1495904+saivenky@users.noreply.github.com>
2022-10-22 11:33:03 -0700

    Cleanup python aliases

--- bash/.bash_aliases/python.sh
+++ bash/.bash_aliases/python.sh
@@ -1,19 +1,36 @@
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+command -v pyenv >/dev/null
+pyenv_installed=$?
+if [ $pyenv_installed -eq 0 ]
+then
+    eval "$(pyenv init -)"
+else
+    echo "Skipping aliases for pyenv (not installed)"
+fi
 
 # poetry
 command -v poetry >/dev/null || export PATH="$HOME/.local/bin:$PATH"
 
 # Python Autocompletion
-export PYTHONSTARTUP=~/.pythonrc
-export PYTHONSTARTUP=~/.pystartup
+# export PYTHONSTARTUP=~/.pythonrc
+# export PYTHONSTARTUP=~/.pystartup
 
-alias ve3="virtualenv -p python3 env"
-alias vac=". ./env/bin/activate"
+function venv_activate() {
+    if [ -d .venv ]
+    then
+        source .venv/bin/activate
+    fi
 
+    if [ -d .env ]
+    then
+        source .env/bin/activate
+    fi
+}
 
-install-ipykernel() {
-    python -m ipykernel install --user --name "$1" --display-name "$2"
+# jupyter
+function install_ipykernel() {
+    name=$1
+    python -m ipykernel install --user --name "${name}"
 }
