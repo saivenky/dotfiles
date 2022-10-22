2fa702bd1d514848bab78b253884cef0feee3cfa
Sai <1495904+saivenky@users.noreply.github.com>
2022-10-22 11:40:51 -0700

    Cleanup misc.sh

--- bash/.bash_aliases/misc.sh
+++ bash/.bash_aliases/misc.sh
@@ -1,12 +1,3 @@
-alias rebase="git pull --rebase origin master"
-alias ve3="virtualenv -p python3 env"
-alias vac=". ./env/bin/activate"
-alias youtube-dl="python3 -m youtube_dl"
-
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
 daysuntil() {
     d1=$(date +%s)
     d2=$(date -d "$1" +%s)
@@ -21,14 +12,4 @@ randtime() {
     d2=$(date -d "$2" +%s)
     rand_time=$(shuf -i $d1-$d2 -n 1)
     date -d @$rand_time
-}
-
-fix-hscroll() {
-    touchpadid=`xinput list | grep "Synaptics TouchPad" | cut -f2 | cut -d"=" -f2`
-    scrolldistpropid=`xinput list-props $touchpadid | grep "Synaptics Scrolling Distance" | cut -f2 | tr -d "[[:alpha:]](): "`
-    scrolldist=`xinput list-props $touchpadid | grep "Synaptics Scrolling Distance" | cut -f3`
-    vscrolldist=`echo $scrolldist | cut -d"," -f1`
-    hscrolldist=`echo $scrolldist | cut -d"," -f2`
-    invhscrolldist=`echo "define abs(x) { if (x < 0) return (-x); return (x) }; abs($hscrolldist) * -1" | bc`
-    xinput set-prop $touchpadid $scrolldistpropid $vscrolldist, $invhscrolldist
 }
