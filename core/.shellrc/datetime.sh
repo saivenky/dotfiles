ac864749c31c9dd2f1497be4ed10ec710bae4876
Sai <1495904+saivenky@users.noreply.github.com>
2023-10-14 12:02:29 -0700

    fix: use gnu date on macos

--- core/.shellrc/datetime.sh
+++ core/.shellrc/datetime.sh
@@ -1,6 +1,16 @@
+
+function gnu_date() {
+  gnu_date_exists=$(command -v gdate)
+  if [ -n "${gnu_date_exists}" ]; then
+    gdate "$@"
+  else
+    date "$@"
+  fi
+}
+
 function daysuntil() {
-    d1=$(date +%s)
-    d2=$(date -d "$1" +%s)
+    d1=$(gnu_date +%s)
+    d2=$(gnu_date -d "$1" +%s)
     total_days=$(( (d2 - d1) / 86400 ))
     weeks=$(( total_days / 7 ))
     days=$(( total_days % 7 ))
@@ -8,8 +18,8 @@ function daysuntil() {
 }
 
 function randtime() {
-    d1=$(date -d "$1" +%s)
-    d2=$(date -d "$2" +%s)
+    d1=$(gnu_date -d "$1" +%s)
+    d2=$(gnu_date -d "$2" +%s)
     rand_time=$(shuf -i $d1-$d2 -n 1)
-    date -d @$rand_time
+    gnu_date -d @$rand_time
 }
